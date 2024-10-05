270f7f0fdcfabf7491941d0a504fd99da5ba438a
Icygoose <hbc0826@gmail.com>
2024-10-06 05:31:07 +0800

    delete useless file

--- UntitledR.R
+++
@@ -1,8 +0,0 @@
-# Read the .Rmd file
-lines <- readLines("project.Rmd")
-
-# Extract lines that contain markdown headings (lines that start with #)
-headings <- grep("^#+", lines, value = TRUE)
-
-# Display or copy the outline
-cat(headings, sep = "\n")
